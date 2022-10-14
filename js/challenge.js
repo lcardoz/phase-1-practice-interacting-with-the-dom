d517c4f855d4ce4a390a7ee8ba6c2a0568527e42
Rayane FassiFihri <rayanefassifihri25@gmail.com>
2022-10-14 16:50:22 +0100

    The button now increments

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,13 @@
-console.log ('hi');
-console.log ("Hellooo")
+// console.log ('hi');
+// console.log ("Hellooo")
 
 document.querySelector('#minus').addEventListener('click', e => {
     const counterElement = document.querySelector('#counter');
     parseInt(counterElement.innerText --);
     
 })
-
+document.querySelector('#plus').addEventListener('click', e => {
+    const counterElement = document.querySelector('#counter');
+    parseInt(counterElement.innerText ++);
+    
+})
